c8372ed3fadbed520688edfbfdc2c059e12e03c3
Harshit Pandey <panditharshit1@gmail.com>
2026-08-11 22:30:00 +0530

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild harshit", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "This Is First", "Second", "Third", "Fourth", "Mild harshit", "Sixth", "Seventh", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
